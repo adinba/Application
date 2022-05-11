a4afc6f4ef1ef5bf136edbe6e53c9f37cbb0ea72
adin <adin.bartoli63@gmail.com>
2022-05-11 14:19:32 +0200

    maj

--- app2.py
+++ app2.py
@@ -499,9 +499,9 @@ else:
     img_file_buffer = st.camera_input("Take a picture")
 
     if img_file_buffer is not None:
-        # To read image file buffer as bytes:
-        bytes_data = img_file_buffer.getvalue()
-        # Check the type of bytes_data:
-        # Should output: <class 'bytes'>
-        st.write(np.array(bytes_data))
+        img = Image.open(img_file_buffer)
+
+        img_array = np.array(img)
+
+        st.image(img_array)
  
